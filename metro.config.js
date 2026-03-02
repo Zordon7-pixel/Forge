a73e98d6143e94a08ec1fa913955232ab805f1c1
Zordon7-pixel <zordon7pixel@users.noreply.github.com>
2026-03-02 00:25:21 -0500

    fix: resolve NativeWind preset + axios browser bundle for RN metro build

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,18 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+// Force axios to use its browser bundle (not the Node.js CJS bundle)
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'axios') {
+    return {
+      filePath: path.resolve(__dirname, 'node_modules/axios/dist/browser/axios.cjs'),
+      type: 'sourceFile',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = withNativeWind(config, { input: './global.css' });
